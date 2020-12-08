c72e7198c339d4af202b220f18bf8eda1c4826aa
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2020-12-09 01:11:02 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Categories from './components/Categories'
+import Categories from './components/Categories';
 import Reminder from './components/Reminder';
+import SignIn from './components/SignIn';
 
-import {useStateValue} from './components/StateProvider';
+import { useStateValue } from './components/StateProvider';
 
 function App() {
+	const [{ activeCategory }, dispatch] = useStateValue();
 
-  const [{activeCategory}, dispatch] = useStateValue();
-
-  return (
-    <div className="app">
-      <Switch>
-        <Route exact path="/">
-          <Navbar title="Home" />
-          <Home />
-        </Route>
-        <Route exact path="/categories">
-          <Navbar title="Categories" />
-          <Categories />
-        </Route>
-        <Route exact path="/reminder">
-          <Navbar title={activeCategory?.length? "Reminders": "Reminders"} />
-          <Reminder />
-        </Route>
-      </Switch>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Switch>
+				<Route exact path='/'>
+					<Navbar title='Home' />
+					<Home />
+				</Route>
+				<Route exact path='/login'>
+					<SignIn />
+				</Route>
+				<Route exact path='/categories'>
+					<Navbar title='Categories' />
+					<Categories />
+				</Route>
+				<Route exact path='/reminder'>
+					<Navbar
+						title={
+							activeCategory?.length ? 'Reminders' : 'Reminders'
+						}
+					/>
+					<Reminder />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
